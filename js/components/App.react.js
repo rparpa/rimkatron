2b8aa2f2a139d25ba1c9f615ef02837bd192bc72
Rémi Parpaillon <remi.parpaillon@gmail.com>
2016-03-20 19:16:32 +0100

    initial commit

--- js/components/App.react.js
+++ js/components/App.react.js
@@ -14,7 +14,7 @@ class App extends Component {
   render() {
     return (
       <div className="wrapper">
-        <img className="logo" src={Logo} />
+        <h1>Rimkatron</h1>
         { this.props.children }
       </div>
     );
--- js/components/pages/HomePage.react.js
+++ js/components/pages/HomePage.react.js
@@ -3,32 +3,47 @@
  * This is the first thing users see of our App
  */
 
-import { asyncChangeProjectName, asyncChangeOwnerName } from '../../actions/AppActions';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
 import { generate } from '../../sentence_generators/ShouldWeTake';
+import Maclouf from '../../../img/maclouf.png';
 
 class HomePage extends Component {
-  render() {
-    const dispatch = this.props.dispatch;
-    const { projectName, ownerName } = this.props.data;
-    return (
-      <div>
-        <h1>Rimkatron</h1>
-        <p>{ generate() }</p>
-      </div>
-    );
-  }
-}
+    constructor(props) {
+      super(props);
+      this.state = { text: generate() };
+      this.handleClick = this.handleClick.bind(this);
+    }
+
+    getInitialState() {
+        return {
+            text: generate()
+        };
+    }
+
+    handleClick(event) {
+        this.setState({text: generate()});
+    }
+
+    render() {
+        var text = this.state.text;
+        return (
+            <div>
+                <p>"{ text }"</p>
+                <img onClick={this.handleClick} className="maclouf" src={ Maclouf } />
+            </div>
+            );
+        }
+    }
 
 // REDUX STUFF
 
 // Which props do we want to inject, given the global state?
 function select(state) {
-  return {
-    data: state
-  };
+    return {
+        data: state
+    };
 }
 
 // Wrap the component to inject dispatch and state into it
